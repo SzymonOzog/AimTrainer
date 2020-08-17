a0334b96ac27b866cd0fcb9aecf68957bd92f6a4
Szymon Ozog <szymon.ozog@gmail.com>
2020-08-17 08:35:06 +0200

    Created Target class and set it's scale to random on BeginPlay

---
+++ Source/AimTrainer/Target.cpp
@@ -0,0 +1,26 @@
+#include "Target.h"
+#include "Components/StaticMeshComponent.h"
+
+ATarget::ATarget()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	staticMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Static Mesh"));
+	SetRootComponent(staticMesh);
+
+}
+
+void ATarget::BeginPlay()
+{
+	Super::BeginPlay();
+	float scale = FMath::RandRange(0.2f, 1.5f);
+	FVector scale3D(scale, scale, scale);
+	SetActorScale3D(scale3D);
+}
+
+void ATarget::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
